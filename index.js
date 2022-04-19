affb70946a60752ab4d70d1542b3fa49f9aa46a0
yesim <sityesim@gmail.com>
2022-04-20 00:05:08 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  
+  if(someValue <= 400){
+    return 'This one is on me!'
+
+  }
+  else if(someValue > 400 && someValue < 2000){
+    return 'That will be twenty bucks.'
+  }
+  else if(someValue > 2000 && someValue <2500 ){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else{
+    return 'No can do.'
+  }
+}
+fun
+
+
+
+function ternaryCheckCity(city) {
+  if(city === "NYC"){
+    return 'Ok, sounds good.'
+  }
+  else{
+    return 'No go.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  if(tip === "generous"){
+    return 'Thank you so much.'
+  }
+  else if(tip === 'not as generous'){
+    return 'Thank you.'
+  }
+  else{return 'Bye.'
+  }
+
 }
